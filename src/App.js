5cf055360aa0cb3ea4ce89f63a73c12f2c05fe0d
tina.semashko <tina.semashko@gmail.com>
2022-07-25 23:05:49 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,32 @@ function App() {
     <div className="App">
       <div className="container">
         <Main defaultCity="Paris" />
+        <footer>
+          <p class="text-center mt-5">
+            <em>
+              This website was coded by{" "}
+              <a
+                href="https://startling-beijinho-552315.netlify.app/about.html"
+                target="_blank"
+                title="about me"
+              >
+                Tina Semashko
+              </a>
+              , and is{" "}
+              <a
+                href="https://github.com/TinaSemashko/weather_react"
+                target="_blank"
+                title="github link that project"
+              >
+                open-sourced on GitHub{" "}
+              </a>
+              and{" "}
+              <a href="https://stately-sopapillas-d6d214.netlify.app/">
+                hosted on Netlify
+              </a>
+            </em>
+          </p>
+        </footer>
       </div>
     </div>
   );
